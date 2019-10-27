f8bd6c075e02e2e00de3ad122dcafbccd7225cb0
ABHINAV KUMAR <ad.abhinavkumar.1999@gmail.com>
2019-10-27 16:50:49 +0530

    color changes

--- app/src/main/java/com/example/todolist/db/TaskContract.java
+++ app/src/main/java/com/example/todolist/db/TaskContract.java
@@ -3,7 +3,7 @@ package com.example.todolist.db;
 import android.provider.BaseColumns;
 
 public class TaskContract {
-    public static final String DB_NAME = "com.aziflaj.todolist.db";
+    public static final String DB_NAME = "com.example.todolist.db";
     public static final int DB_VERSION = 1;
 
     public class TaskEntry implements BaseColumns {
